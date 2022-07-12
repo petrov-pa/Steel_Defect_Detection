b27551e222d8bfd93111221fda03b0e981ab377d
Pavel <narfblop@gmail.com>
2022-07-12 08:55:35 +0300

    flask

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import numpy as np
 from models import FixedDropout
 import cv2
 import os
-from albumentations import Normalize
 from tensorflow.keras.models import load_model
 
 
@@ -11,32 +10,27 @@ def run():
     if not list_img:
         return print('Нет файлов в папке')
     # загружаем обученную модель
-    # clf = get_clf()
-    # linknet = get_linknet()
-    # clf.load_weights('./models/best_classifier.hdf5')
-    # linknet.load_weights('./models/best_linknet.hdf5')
     clf = load_model('./models/clf.h5', custom_objects={'FixedDropout': FixedDropout}, compile=False)
     linknet = load_model('./models/linknet.h5', custom_objects={'FixedDropout': FixedDropout}, compile=False)
-    normalize = Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
-    for img in list_img:
-        orig_img = cv2.imread('./inputs/' + img)
+    for name in list_img:
+        orig_img = cv2.imread('./inputs/' + name)
         if orig_img is None:
-            return print('Неверный формат файла: {}'.format(img))
-        img_for_clf = normalize(image=orig_img)['image']
-        img_for_seg = orig_img/255
+            return print('Неверный формат файла: {}'.format(name))
+        norm_img = orig_img/255
+        img_part = [norm_img[:, 320 * part:320 * (part + 1), :] for part in range(5)]
         # предсказываем наличие дефекта
-        pred_clf = clf.predict(np.array([img_for_clf[:, 320 * part:320 * (part + 1), :] for part in range(5)]))
-        if np.sum(pred_clf > 0.5)+1:  # если дефект есть, то делаем сегментацию
-            pred_seg = linknet.predict(np.array([img_for_seg[:, 320 * part:320 * (part + 1), :] for part in range(5)]))
+        pred_clf = clf.predict(np.array(img_part))
+        if np.sum(pred_clf > 0.5):  # если дефект есть, то делаем сегментацию
+            pred_seg = linknet.predict(np.array(img_part))
             full_pred = np.hstack([pred_seg[0], pred_seg[1], pred_seg[2], pred_seg[3], pred_seg[4]])
             pred_mask = (full_pred > 0.5).astype(np.int32)
-            pred_mask = pred_mask * 200
+            pred_mask = pred_mask * 100
             pred_mask[:, :, 0] = pred_mask[:, :, 0] + pred_mask[:, :, 3]
             pred_mask = pred_mask[:, :, :-1]
         else:
             pred_mask = np.zeros((256, 1600, 1))
         # записываем в файл
-        cv2.imwrite('./outputs/' + img, orig_img+pred_mask)
+        cv2.imwrite('./outputs/' + name, orig_img+pred_mask)
     return print('Сегментация закончена')
 
 
