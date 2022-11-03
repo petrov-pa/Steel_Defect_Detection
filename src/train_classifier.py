1518218a4e3ec29c6ef16ace6747b3c86e8123a7
Pavel Petrov <64748758+petrov-pa@users.noreply.github.com>
2022-11-03 15:35:01 +0300

    Update train_classifier.py

--- src/train_classifier.py
+++ src/train_classifier.py
@@ -20,4 +20,3 @@ callbacks = [lr_callback, save_callback]
 clf = get_clf()
 clf.compile(optimizer=Adam(0.001), loss='binary_crossentropy', metrics=['accuracy'])
 clf.fit(train_gen_clf, validation_data=test_gen_clf, epochs=80, callbacks=callbacks)
-
